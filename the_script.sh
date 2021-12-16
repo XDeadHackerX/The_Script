2af6b662cbe16e76cccd23daa5b577428f96d0ec
DEAD <aitor.rosello11@gmail.com>
2021-12-16 18:59:19 -0500

    Add files via upload

--- the_script.sh
+++ the_script.sh
@@ -10,7 +10,7 @@ echo "	       ░░░██║░░░██╔══██║██╔══
 echo "	       ░░░██║░░░██║░░██║███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░"
 echo "	       ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░"
 echo 
-echo "		     		   Create for: XDeadHackerX v1.0"
+echo "		     		   Create for: XDeadHackerX v2.0"
 echo "	-----------------------------------------------------------------------------------"
 echo " 	Any actions and or activities related to The_Script is solely your responsibility"
 echo "	-----------------------------------------------------------------------------------"
@@ -23,9 +23,10 @@ echo "		     -----------------------------------------------"
 echo "		     Elige una de las siguientes opciones: "
 echo "		     0-Instalar requisitos"
 echo "		     1-Informacion sobre tarjeta de Red + Velocidad"
-echo "		     2-Encontrar Redes Sociales"
+echo "		     2-Informacion de una web"
 echo "		     3-Informacion de un numero de Telefono"
-echo "		     4-Salir"
+echo "		     4-Encontrar Redes Sociales"
+echo "		     5-Salir"
 echo "		     -----------------------------------------------"
 echo
 read -p " Elige una opcion: " opc
@@ -38,5 +39,7 @@ read -p " Elige una opcion: " opc
 				;;
 			3 )	bash requisitos/3.sh
 				;;
-			4 )	exit
+			4 )	bash requisitos/4.sh
+				;;
+			5 )	exit
 	esac
