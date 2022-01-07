772b9b6a418d5e7d1059d435e2763ce8e92f61fc
DEAD <aitor.rosello11@gmail.com>
2022-01-07 14:56:18 -0500

    Update 0.sh

--- requisitos/0.sh
+++ requisitos/0.sh
@@ -3,6 +3,7 @@
 sudo apt-get install curl
 sudo apt install python-pip
 sudo apt install python3-pip
+sudo apt install git
 sudo apt-get install cat
 sudo apt-get install hostname
 sudo curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
