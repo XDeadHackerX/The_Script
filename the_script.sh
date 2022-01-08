ede98c960556e7e2cdd3bd307adc031305d32bb3
DEAD <aitor.rosello11@gmail.com>
2022-01-08 13:27:36 -0500

    Add files via upload

--- the_script.sh
+++ the_script.sh
@@ -3,32 +3,32 @@
 clear
 echo
 echo
-echo "	       ████████╗██╗░░██╗███████╗░██████╗░█████╗░██████╗░██╗██████╗░████████╗"
-echo "	       ╚══██╔══╝██║░░██║██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝"
-echo "	       ░░░██║░░░███████║█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░"
-echo "	       ░░░██║░░░██╔══██║██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░"
-echo "	       ░░░██║░░░██║░░██║███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░"
-echo "	       ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░"
+echo "	           ████████╗██╗░░██╗███████╗░██████╗░█████╗░██████╗░██╗██████╗░████████╗"
+echo "	           ╚══██╔══╝██║░░██║██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝"
+echo "	           ░░░██║░░░███████║█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░"
+echo "	           ░░░██║░░░██╔══██║██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░"
+echo "	           ░░░██║░░░██║░░██║███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░"
+echo "	           ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░"
 echo
-echo "				           (⌐■_■)– ︻デ═一"
-echo "			 __________________________________________________"					
-echo "		     	   ︻デ═一  Created by: XDeadHackerX v4.0  ︻デ═一 " 
-echo "     -------------------------------------------------------------------------------------------"
-echo "     Cualquier acción y o actividad relacionada con The_Script es únicamente su responsabilidad"
-echo "     -------------------------------------------------------------------------------------------"
+echo "				               (⌐■_■)– ︻デ═一"
+echo "			     __________________________________________________"					
+echo "		     	       ︻デ═一  Created by: XDeadHackerX v5.0  ︻デ═一 " 
+echo "         -------------------------------------------------------------------------------------------"
+echo "         Cualquier acción y o actividad relacionada con The_Script es únicamente su responsabilidad"
+echo "         -------------------------------------------------------------------------------------------"
 echo
 echo
 echo "	[*] Elige una de las siguientes opciones: "
-echo "	--------------------------------------------------------------------------------------"
-echo "	0º Instalar requisitos        		                (Marcar la 1º vez)	      "
-echo "	1º Informacion sobre la tarjeta de Red + Velocidad	(Informacion importante)      "
-echo "	2º Informacion de una Pagina Web		        (OSINT)			      "
-echo "	3º Informacion de una empresa				(Emails/Trabajadores/Dominios)"
-echo "	4º Informacion de un numero de Telefono  		(Funciona mal)		      "
-echo "	5º Encontrar Redes Sociales    			        (No es certero al 100%)	      "
-echo "	6º Metadatos		               	                (Extraerlos y o eliminarlos)  "
-echo "	7º ☢Salir☢					        (Salir del programa)	      "
-echo "	--------------------------------------------------------------------------------------"
+echo "	--------------------------------------------------------------------------------------------"
+echo "	0º Instalar requisitos        		                (Marcar la 1º vez)	            "
+echo "	1º Informacion sobre la tarjeta de Red + Velocidad	(Toda la Informacion necesaria)     "
+echo "	2º Informacion de una Pagina Web		        (Info tecnologias y del propietario)"
+echo "	3º Informacion de una Empresa				(Emails/Trabajadores/Dominios)      "
+echo "	4º Encontrar Redes Sociales    			        (No es certero al 100%)	            "
+echo "	5º Escanear nuestra red		               	        (Ver los host conectados)           "
+echo "	6º Metadatos		               	                (Extraerlos y o eliminarlos)        "
+echo "	8º ☢Salir☢					        (Salir del programa)	            "
+echo "	--------------------------------------------------------------------------------------------"
 echo
 read -p " [*] Elige una opcion: " opc
 	case $opc in
@@ -38,13 +38,13 @@ read -p " [*] Elige una opcion: " opc
 				;;
 			2 )	bash requisitos/2.sh
 				;;
-			3 )	bash requisitos/6.sh
+			3 )	bash requisitos/3.sh
 				;;
-			4 )	bash requisitos/3.sh
+			4 )	bash requisitos/4.sh
 				;;
-			5 )	bash requisitos/4.sh
+			5 )	bash requisitos/5.sh
 				;;
-			6 )	bash requisitos/5.sh
+			6 )	bash requisitos/6.sh
 				;;
 			7 )	exit
 	esac
